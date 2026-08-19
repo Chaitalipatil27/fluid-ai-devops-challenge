bc2aa5da8f91be734e1d45b7620162466ba2edbd
Chaitali Patil <chaitalip976@gmail.com>
2026-08-19 20:19:57 +0000

    Initial Kubernetes DevOps project

---
+++ app/main.py
@@ -0,0 +1,40 @@
+import os
+import psycopg2
+from fastapi import FastAPI
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST", "postgres")
+DB_NAME = os.getenv("DB_NAME", "appdb")
+DB_USER = os.getenv("DB_USER", "appuser")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "apppassword")
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "DevOps Kubernetes CI/CD application is running"
+    }
+
+
+@app.get("/health")
+def health():
+    try:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+        conn.close()
+
+        return {
+            "status": "healthy",
+            "database": "connected"
+        }
+
+    except Exception:
+        return {
+            "status": "unhealthy",
+            "database": "disconnected"
+        }
